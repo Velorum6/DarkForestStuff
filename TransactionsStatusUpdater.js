9c672e06f831d4396e2ae3496e882b8187493329
Velorum <88809256+Velorum6@users.noreply.github.com>
2021-08-19 11:14:32 +0100

    Update TransactionsStatusUpdater.js

--- TransactionsStatusUpdater.js
+++ TransactionsStatusUpdater.js
@@ -5,22 +5,46 @@ function updateTxStatus(){
 df.contractsAPI.contractCaller.queue.invocationIntervalMs=100; 
 }
 
+// This is the function that shows the transactions in the console
+function unconfirmed(){
+console.log('Moves : ' , df.getUnconfirmedMoves().length)
+console.log('Upgrades : ' , df.getUnconfirmedUpgrades().length)
+console.log('Wormhole : ' , df.getUnconfirmedWormholeActivations().length) 
+}
+
+// showTransactions is the text inside the plugin
+let showTransactions = document.createElement('div');
+
+// This is a function that updates the text of showTransactions to the current unconfirmed transactions
+function updateTx(){
+showTransactions.innerText = 'Moves :  ' + df.getUnconfirmedMoves().length + '\nUpgrades :  ' +     df.getUnconfirmedUpgrades().length + '\nWormhole :  ' + df.getUnconfirmedWormholeActivations().length;
+}
+
 // And this is the design of the plugin from the user perspective
 class Plugin {
 
     constructor() { }
 
     async render(div) {
-        div.style.width = '90px';
-        div.style.height = '90px';
-        const myButton = document.createElement('button');
-        myButton.innerText = 'Update';
-        myButton.addEventListener('click', () => {
+        // This button update the status of the transactions
+        const updateTxStatusButton = document.createElement('button');
+        updateTxStatusButton.innerText = 'Update';
+        updateTxStatusButton.addEventListener('click', () => {
             updateTxStatus();
         });
-        div.appendChild(myButton);
+        // This button refresh the unconfirmed transactions
+        const unconfirmedButton = document.createElement('button');
+        unconfirmedButton.innerText = 'Transaction';
+        unconfirmedButton.addEventListener('click', () => {
+            unconfirmed();
+            updateTx();
+        });
+        
+        div.appendChild(updateTxStatusButton);
+        div.appendChild(unconfirmedButton);
+        div.appendChild(showTransactions);
     }
-    
+
     destroy() { }
 }
 
