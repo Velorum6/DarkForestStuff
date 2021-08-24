61010888cf4a44a1a395543b036fc57bc1c32ac7
Velorum <88809256+Velorum6@users.noreply.github.com>
2021-08-24 18:56:12 +0100

    Update JSFunctions.js

--- ArtifactsData/JSFunctions.js
+++ ArtifactsData/JSFunctions.js
@@ -1,4 +1,4 @@
-//functions to apply to the data gathered by the subgraph to count automatically all the amount of artifacts
+//Functions to apply to the data gathered by the subgraph to count automatically all the amount of artifacts
 
 
 //This function create a new array where if an artifact that gets lost when used (if a.artifactType === "Type") is used (if a.lastDeactivated !== 0) then is not added to the new array
@@ -20,10 +20,28 @@ console.log(newArtifactArray)
 
 
 
-//This functions just counts the number of each type of artifacts in the new array
+//This function just counts the number of each type of artifacts in the new array
 var artifacts = {};
 for (var a of newArtifactArray) {
     if (!artifacts[a.artifactType]) artifacts[a.artifactType] = 0;
     artifacts[a.artifactType]++;
 }
 console.log(artifacts)
+
+
+//This function create a new array with the destroyed Artifacts
+var destroyedArtifacts = []
+for (var a of d.data.artifacts){
+  if (a.lastDeactivated !== 0) {
+    if (a.artifactType === "PHOTOIDCANNON") { destroyedArtifacts.push(a);
+    } else if (a.artifactType === "PLANETARYSHIELD") { destroyedArtifacts.push(a); 
+    } else if (a.artifactType === "BLOOMFILTER") { destroyedArtifacts.push(a);
+    } else if (a.artifactType === "BLACKDOMAIN") { destroyedArtifacts.push(a);
+    } else {continue;
+    }
+  }
+  else {continue;
+  }
+}
+
+console.log(destroyedArtifacts)
