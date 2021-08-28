419bbd86f723f7254a9a4da32c89ada7e928c57b
Velorum <88809256+Velorum6@users.noreply.github.com>
2021-08-28 14:16:56 +0200

    Create ArtifactRarity.js

---
+++ Plugins/ArtifactRarity.js
@@ -0,0 +1,81 @@
+//A plugin that calls directly, functions made by modukon
+
+var GRAPH_API_URL = "https://api.thegraph.com/subgraphs/name/darkforest-eth/dark-forest-v06-round-3";
+
+async function getGraphQLData(graphApiUrl, query) {
+	const response = await fetch(graphApiUrl, {
+		method: "POST",
+		body: JSON.stringify({ query }),
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json"
+		}
+	});
+	const json = await response.json();
+	return json;
+};
+
+function getArtifactQueryStr(rarity, lastId) {
+	return `{
+		artifacts(first: 1000, where: {rarity: ${rarity}, id_gt: "${lastId}"}) {
+			rarity
+			artifactType
+			lastDeactivated
+			id
+			discoverer {id}
+		}
+	}`;
+}
+
+async function getArtifacts(rarity) {
+	let artifacts = [];
+	let lastId = 0;
+	console.log("downloading "+rarity+" artifacts from graph...");
+	for (let i = 0; ; i++) {
+		const graphQuery = getArtifactQueryStr(rarity, lastId);
+		const graphResponse = await getGraphQLData(GRAPH_API_URL, graphQuery);
+		let arr = graphResponse.data.artifacts;
+		if (arr.length === 0) break;
+		console.log("downloaded "+arr.length+" artifacts");
+		lastId = arr[arr.length-1].id;
+		artifacts.push(...arr);
+	}
+	console.log("download complete! "+artifacts.length+" "+rarity+" artifacts");
+	return artifacts;
+};
+
+function artifactTypeGetsDestroyed(type) {
+	return (type === "PHOTOIDCANNON" || type === "BLACKDOMAIN" || type === "BLOOMFILTER" || type === "PLANETARYSHIELD");
+}
+
+function artifactWasDestroyed(artifact) {
+	if (artifact.lastDeactivated === 0) return false;	
+	if (artifactTypeGetsDestroyed(artifact.artifactType)) return true;
+	return false;
+}
+
+async function logArtifactInfoForRarity(rarity) {
+	let artifacts = await getArtifacts(rarity);
+	let artifactTypes = {};
+	let destroyedArtifactTypes = {};
+	let destroyedArtifactCount = 0;
+	let notDestroyedArtifactCount = 0;
+	for (let a of artifacts) {
+		let type = a.artifactType;
+		
+		if (!artifactTypes[type]) artifactTypes[type] = 1;
+		else artifactTypes[type]++;
+		
+		if (artifactWasDestroyed(a)) {
+			destroyedArtifactCount++;
+			if (!destroyedArtifactTypes[type]) destroyedArtifactTypes[type] = 1;
+			else destroyedArtifactTypes[type]++;
+		} else notDestroyedArtifactCount++;
+		
+	}
+	console.log("artifactTypes", artifactTypes);
+	console.log("destroyedArtifactTypes", destroyedArtifactTypes);
+	console.log(artifacts.length+" artifacts, "+destroyedArtifactCount+" destroyed, "+notDestroyedArtifactCount+" not destroyed");
+}
+
+logArtifactInfoForRarity("MYTHIC");
